5dfeab24d54e8d66fd71dbf7d76f39b58ceab900
cacerra1 <claudia.acerra@gmail.com>
2019-07-25 15:45:06 -0400

    Another Git Committ

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,21 +1,31 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
-    """ Test API View"""
 
-    def get(self, request, format=None):
-        """ Returns a list of APIView features"""
+    def get(self, request, format = None):
+        """Returns a list of APIView features"""
 
-        an_apiview =[
-        "Uses HTTP Methos as funtions\
-        (get, post, put, patch, delete)",
-        "It Similar to a traditional Django View",
-        "Gives you the most control over your application logic",
-        "Is mapped manually to URLS"
-
+        an_apiview = [
+            'Will you work?????', "Who knows",
         ]
 
-        # must retrun a Response
-        return Response({"message": 'Hello!', 'an_apiview': an_apiview})
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = name
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
